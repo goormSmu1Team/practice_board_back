d2e815980c522d1562d205fa786af3b13a38ffee
y22jun <tlsdpwns0612@naver.com>
2024-09-11 15:16:22 +0900

    Pull Request

---
+++ src/main/java/groom/goorm_board_back/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package groom.goorm_board_back.repository;
+
+import groom.goorm_board_back.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByBoardId(Long boardId);
+
+}
+

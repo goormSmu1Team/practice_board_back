48c32721d05373d5d4dc2ad31c76cc3f3a22b35f
jwoo13 <nachanee12@naver.com>
2024-09-14 22:39:56 +0900

    DB수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO board (title, content, created_date, view_count) VALUES ('제목1', '내용1', NOW(), 0);
 INSERT INTO board (title, content, created_date, view_count) VALUES ('제목2', '내용2', NOW(), 0);
 INSERT INTO board (title, content, created_date, view_count) VALUES ('제목3', '내용3', NOW(), 0);
+
